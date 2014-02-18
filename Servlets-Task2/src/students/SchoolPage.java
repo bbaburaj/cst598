b3c00bdaf6ee1d4edf9df710eb3e5bccc25edfe5
bbaburaj <bbaburaj@asu.edu>
2014-02-18 14:56:59 -0700

    Updated web.xml and servlet action names

---
+++ Servlets-Task2/src/students/SchoolPage.java
@@ -0,0 +1,36 @@
+package students;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SchoolPage extends HttpServlet{
+	private static String file = null;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		file = config.getInitParameter("student_list");
+		if (file == null || file.length() == 0) {
+			throw new ServletException();
+		}
+		System.out.println("Loaded init param student_info with " + file);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><HEAD><TITLE> Student Information Record </TITLE></HEAD><BODY>");
+		String school = request.getParameter("school");
+		System.out.println(school);
+		out.println("</BODY></HTML>");
+	}
+}
---
+++ Servlets-Task2/src/students/WeekPage.java
@@ -0,0 +1,40 @@
+package students;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WeekPage extends HttpServlet{
+	private static String file = null;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		file = config.getInitParameter("student_list");
+		if (file == null || file.length() == 0) {
+			throw new ServletException();
+		}
+		System.out.println("Loaded init param student_info with " + file);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><HEAD><TITLE> Student Information Record </TITLE></HEAD><BODY>");
+
+		String[] availability = request.getParameterValues("availability");
+
+		System.out.println(availability);
+
+		
+		out.println("</BODY></HTML>");
+	}
+}
