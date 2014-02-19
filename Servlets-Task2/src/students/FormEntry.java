2d9f270e5cf571cb6c3598fde7b74236180cc236
bbaburaj <bbaburaj@asu.edu>
2014-02-18 23:49:32 -0700

    Updated with stateful behavior

---
+++ Servlets-Task2/src/students/FormEntry.java
@@ -0,0 +1,152 @@
+package students;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.*;
+
+public class FormEntry {
+	public static final String STUDENT_FILE = "student.txt";
+	public static final String NEW_LINE = System.getProperty("line.separator");
+	private static String searchFirstName = "firstname";
+	private static String searchLastName = "lastname";
+	private static String searchLanguages = "languages";
+	private static String searchAvailability = "days";
+	private static String searchSchool = "school";
+	
+	private Map<String, StudentInfo> sInfoMap = new Hashtable<String, StudentInfo>();
+
+	public FormEntry() throws IOException {
+		this(STUDENT_FILE); 
+	}
+	public FormEntry(String fname) throws IOException {
+		this(new BufferedReader(new FileReader(fname)));
+	}
+	
+	public FormEntry(InputStream st) throws IOException{
+		this(new BufferedReader(new InputStreamReader(st)));
+	}
+	
+	private FormEntry(BufferedReader br) throws IOException {	
+		String fName = null;
+		String lName = null;
+		String id = null;
+		String[] languagesKnown = null;
+		String[] daysAvailable = null;
+		String school = null;
+
+		try {
+			String nextLine = null;
+			while ( (nextLine=br.readLine()) != null)
+			{
+				id  = nextLine;
+				fName = br.readLine();
+				lName = br.readLine();
+				languagesKnown = br.readLine().split(",");
+				daysAvailable = br.readLine().split(",");
+				school = br.readLine();
+				addInfo(fName, lName, languagesKnown, daysAvailable, id, school);
+			}
+			br.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Error processing student entries");
+			throw new IOException("Failed to process student.txt");
+		}
+	}
+
+	public void addInfo(String fname, String lname, String[] lang, String[] days, String id, String school)
+	{ 
+		addInfo(id, new StudentInfo(fname, lname, lang, days, id,school));
+	}
+
+	public void addInfo(String number, StudentInfo newEntry)
+	{ sInfoMap.put(number, newEntry);}
+	
+	public void updateStudentFile(String name) throws IOException, URISyntaxException{
+		File f = new File(name);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(f.getAbsoluteFile()));
+		Iterator<StudentInfo> i = sInfoMap.values().iterator();
+		while(i.hasNext()){
+			StudentInfo student = i.next();
+			writer.write(student.getStudentId()+NEW_LINE);
+			writer.write(student.getFirstName()+NEW_LINE);
+			writer.append(student.getLastName()+NEW_LINE);
+			writer.append(student.getLanguagesKnown()+NEW_LINE);
+			writer.append(student.getDaysAvailable()+NEW_LINE);
+			writer.append(student.getSchool()+NEW_LINE);
+		}
+		writer.close();
+	}
+	
+	public int getTotalStudents(){
+		return sInfoMap.size();
+	}
+	
+	public List<String> listAllInformation(){
+		List<String> output = new ArrayList<String>();
+		Iterator<StudentInfo> it = sInfoMap.values().iterator();
+		while(it.hasNext()){
+			StudentInfo st = it.next();
+			output.add(st.toString());
+		}
+		return output;
+	}
+	
+	public List<String> searchEntry(String qString){
+		List<String> result = new ArrayList<String>();
+		Iterator<StudentInfo> it =sInfoMap.values().iterator();
+		StringTokenizer tokenizer =null;
+		while(it.hasNext()){
+			StudentInfo st = it.next();
+			tokenizer = new StringTokenizer(qString,"&");
+			String output = "";
+			while(tokenizer.hasMoreTokens()){
+				String[] searchFor = tokenizer.nextToken().split("=");
+				System.out.println(searchFor[0]+" "+searchFor[1]);
+				if(searchFor[0].equalsIgnoreCase(searchFirstName)){
+					if(st.getFirstName().contains(searchFor[1]))output = st.toString();
+					else{break;}
+				} 
+				if(searchFor[0].equalsIgnoreCase(searchLastName)){
+					if(st.getLastName().contains(searchFor[1]))output = st.toString();
+					else{break;}
+				} 
+				if(searchFor[0].equalsIgnoreCase(searchSchool)){
+					searchFor[1] = searchFor[1].replaceAll("%20", " ");
+					if(st.getSchool().contains(searchFor[1]))output = st.toString();
+					else{break;}
+				} 
+				if(searchFor[0].equalsIgnoreCase(searchLanguages)){
+					String lang[] = searchFor[1].split("\\+");
+					output="";
+					for(String s:lang){
+						if(st.getLanguagesKnown().contains(s)){
+							output = st.toString();
+						}
+					} 
+					if(output.length()==0)break;
+					
+				}
+				if(searchFor[0].equalsIgnoreCase(searchAvailability)){
+					String days[] = searchFor[1].split("\\+");
+					output="";
+					for(String s:days){
+						if(st.getDaysAvailable().contains(s)){
+							output = st.toString();
+						}
+					}
+					if(output.length()==0)break;
+				}
+			}
+			if (output.length()>0)result.add(output);
+		}
+		return result;
+		
+	}
+	
+
+}
---
+++ Servlets-Task2/src/students/StudentInfo.java
@@ -0,0 +1,88 @@
+package students;
+
+import java.util.List;
+
+public class StudentInfo {
+	private String firstName;
+	private String lastName;
+	private String[] languagesKnown;
+	private String[] daysAvailable;
+	private String studentId;
+	private String school;
+	
+	public StudentInfo(){
+		
+	}
+
+	public StudentInfo(String firstName, String lastName,
+			String[] languagesKnown, String[] daysAvailable, String id, String school) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.languagesKnown = languagesKnown;
+		this.daysAvailable = daysAvailable;
+		this.studentId = id;
+		this.school = school;
+	}
+
+	public String toString() {
+		return "Student ID:"+this.studentId+", Name:"+ this.firstName + " " + this.lastName+
+				" knows:" + this.getLanguagesKnown()+" is from:"+this.getSchool()
+				+ "\r\nis free on:" + this.getDaysAvailable();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getLanguagesKnown() {
+		String languages = "";
+		for (String s : languagesKnown) {
+			languages = languages.concat(s.concat(","));
+		}
+		return languages.substring(0,languages.length()-1);
+	}
+
+	public String getDaysAvailable() {
+		String days = "";
+		for (String s : daysAvailable) {
+			days = days.concat(s.concat(","));
+		}
+		return days.substring(0,days.length()-1);
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+	public String getSchool(){
+		return school;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setLanguagesKnown(String[] languagesKnown) {
+		this.languagesKnown = languagesKnown;
+	}
+
+	public void setDaysAvailable(String[] daysAvailable) {
+		this.daysAvailable = daysAvailable;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+}
+
